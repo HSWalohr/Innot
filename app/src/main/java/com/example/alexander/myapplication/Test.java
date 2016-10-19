10a3b449588cddebf183ba48ced55f4f6cce167a
HSWalohr <al-161385@hs-weingarten.de>
2016-10-19 11:16:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/alexander/myapplication/Test.java
@@ -0,0 +1,11 @@
+package com.example.alexander.myapplication;
+
+/**
+ * Created by Alexander on 15.10.2016.
+ */
+
+public class Test {
+    public String callMe(){
+        return "Einstellungen noch nicht implementiert";
+    }
+}
